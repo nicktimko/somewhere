204fdbae2d38df356b896403dccebed44e327a23
Nick Timkovich <prometheus235@gmail.com>
2016-06-11 22:47:12 -0500

    PEP8 tidying-up

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ Makes use of the PATH variable and the current directory."""
 
 setuptools.setup(
     name="where",
-    packages = ["where"],
+    packages=["where"],
     version="1.0.1",
     description=description,
-    author = 'Henry Weickert',
-    author_email = 'henryweickert@gmail.com',
-    url = 'https://github.com/hweickert/where',
-    keywords = ['where', 'which'],
+    author='Henry Weickert',
+    author_email='henryweickert@gmail.com',
+    url='https://github.com/hweickert/where',
+    keywords=['where', 'which'],
 )
--- where/__init__.py
+++ where/__init__.py
@@ -2,33 +2,29 @@ from __future__ import absolute_import
 
 import os
 import platform
-import itertools
 
 
-def where( filename ):
-    """ Returns all matching file paths. """
-
+def where(filename):
+    """Returns all matching file paths."""
     return list(iwhere(filename))
 
 
-def first( filename ):
-    """ Returns first matching file path. """
-
+def first(filename):
+    """Returns first matching file path."""
     try:
         return next(iwhere(filename))
     except StopIteration:
         return None
 
 
-def iwhere( filename ):
-    """ Like where() but returns an iterator. """
-
-    possible_paths =      _gen_possible_matches( filename )
-    existing_file_paths = ( p for p in possible_paths if os.path.isfile(p) )
+def iwhere(filename):
+    """Like where() but returns an iterator."""
+    possible_paths = _gen_possible_matches(filename)
+    existing_file_paths = (p for p in possible_paths if os.path.isfile(p))
     return existing_file_paths
 
 
-def _gen_windows_matches( paths ):
+def _gen_windows_matches(paths):
     for path in paths:
         yield path
         yield path + ".bat"
@@ -36,15 +32,14 @@ def _gen_windows_matches( paths ):
         yield path + ".exe"
 
 
-def _gen_possible_matches( filename ):
-    path_parts =     os.environ.get("PATH", "").split( os.pathsep )
-    path_parts =     itertools.chain( (os.curdir,), path_parts )
-    possible_paths = ( os.path.join(path_part, filename) for path_part in path_parts )
+def _gen_possible_matches(filename):
+    path_parts = [os.curdir] + os.environ.get("PATH", "").split(os.pathsep)
+    possible_paths = (os.path.join(pp, filename) for pp in path_parts)
 
     if platform.system() == "Windows":
         possible_paths = _gen_windows_matches(possible_paths)
 
-    possible_paths = ( os.path.abspath(p) for p in possible_paths )
+    possible_paths = (os.path.abspath(p) for p in possible_paths)
 
     results = set()
     for result in possible_paths:
